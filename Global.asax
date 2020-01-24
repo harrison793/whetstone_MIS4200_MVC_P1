7027f824a39891308beca7207001a2a5489cdc54
Whetstone <hw075215@ohio.edu>
2020-01-24 11:33:46 -0500

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="whetstone_MIS4200_MVC_P1.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(whetstone_MIS4200_MVC_P1.Startup))]
+namespace whetstone_MIS4200_MVC_P1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
