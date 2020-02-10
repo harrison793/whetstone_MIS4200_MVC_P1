ca0c18b83245aa8571183c26024442e05bdf2e8e
Harrison Whetstone <hw075215@ohio.edu>
2020-02-10 16:31:00 -0500

    Changes to MIS4200Context.cs and Web.config

--- DAL1/MIS4200Context.cs
+++ DAL1/MIS4200Context.cs
@@ -7,7 +7,20 @@ using System.Data.Entity;
 
 namespace whetstone_MIS4200_MVC_P1.DAL1
 {
-    public class MIS4200Context
+    public class MIS4200Context : DbContext 
     {
+        public MIS4200Context() : base("name=DefaultConnection")
+        {
+            // this method is a 'constructor' and is called when a new context is created
+            // the base attribute says which connection string to use
+        }
+
+        // Include each object here. The value inside <> is the name of the class,
+        // the value outside should generally be the plural of the class name
+        // and is the name used to reference the entity in code
+        public DbSet<Appointment> Appointments { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Procedure> Procedures { get; set; }
+        public DbSet<appointmentDetail> AppointmentDetails { get; set; }
     }
 }
