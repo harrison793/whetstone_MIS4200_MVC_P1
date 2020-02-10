46be6d000e0d9774de4dcecd83e5470bf3e2e8e0
Harrison Whetstone <hw075215@ohio.edu>
2020-02-10 09:59:16 -0500

    Changed the models, patient-procedure

---
+++ DAL/MIS_4200_Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace whetstone_MIS4200_MVC_P1.DAL
+{
+    public class MIS_4200_Context
+    {
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+    }
+}
--- Models/Customer.cs
+++ Models/Customer.cs
@@ -5,14 +5,14 @@ using System.Web;
 
 namespace whetstone_MIS4200_MVC_P1.Models
 {
-    public class Customer
+    public class Patient
     {
-        public int customerID { get; set; }
+        public int patientID { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
         public string email { get; set; }
         public string phone { get; set; }
-        public DateTime customerSince { get; set; }
+        public DateTime patientSince { get; set; }
         // add any other fields as appropriate
         // a customer can have any number of orders, a 1:M relationship,
         // We represent this in the model with an ICollection
@@ -20,47 +20,47 @@ namespace whetstone_MIS4200_MVC_P1.Models
         // (the name inside the <> is the object name),
         // and the local name of the collection will be Order
         // (the object name and the local name do not have to be the same)
-        public ICollection<Order> Order { get; set; }
+        public ICollection<Appointment> Appointment { get; set; }
     }
 
-    public class Order
+    public class Appointment
     {
-        public int orderID { get; set; }
+        public int appointmentID { get; set; }
         public string description { get; set; }
-        public DateTime orderDate { get; set; }
+        public DateTime appointmentDate { get; set; }
         // add any other fields as appropriate
         //Order is on the "one" side of a one-to-many relationship with OrderDetail
         //and we indicate that with an ICollection
-        public ICollection<orderDetail> OrderDetail { get; set; }
+        public ICollection<appointmentDetail> appointmentDetail { get; set; }
         //Order is on the Many side of the one-to-many relation between Customer
         //and Order and we represent that relationship like this
-        public int customerID { get; set; }
-        public virtual Customer Customer { get; set; }
+        public int patientID { get; set; }
+        public virtual Patient Patient { get; set; }
     }
 
-    public class Product
+    public class Procedure
     {
-        public int productID { get; set; }
+        public int procedureID { get; set; }
         public string description { get; set; }
         public decimal unitCost { get; set; }
         // add any other fields as appropriate
         //Product is on the "one" side of a one-to-many relationship with OrderDetail
         //we indicate that with an ICollection
-        public ICollection<orderDetail> OrderDetail { get; set; }
+        public ICollection<appointmentDetail> appointmentDetail { get; set; }
     }
 
 
-    public class orderDetail
+    public class appointmentDetail
     {
-        public int orderdetailID { get; set; }
-        public int qtyOrdered { get; set; }
+        public int appointmentDetailID { get; set; }
+        public int unitCost{ get; set; }
         public decimal price { get; set; }
         // the next two properties link the orderDetail to the Order
-        public int orderID { get; set; }
-        public virtual Order Order { get; set; }
+        public int procedureID { get; set; }
+        public virtual Procedure Procedure { get; set; }
         // the next two properties link the orderDetail to the Product
         public int productID { get; set; }
-        public virtual Product Product { get; set; }
+        public virtual Procedure Product { get; set; }
     }
 
 
