6bf84ba11bf67388102f0725ab2accf9fe86136e
Harrison Whetstone <hw075215@ohio.edu>
2020-02-10 10:36:25 -0500

    made DAL folder

---
+++ DAL1/MIS4200Context.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using whetstone_MIS4200_MVC_P1.Models; // This is needed to access the models
+using System.Data.Entity;
+
+namespace whetstone_MIS4200_MVC_P1.DAL1
+{
+    public class MIS4200Context
+    {
+    }
+}
