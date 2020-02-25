4084e0e43c1f510764ea1f8f7d24492dac9576d8
Harrison Whetstone <hw075215@ohio.edu>
2020-02-25 09:28:11 -0500

    changes to mis4200  context back to patient relationship

--- DAL1/MIS4200Context.cs
+++ DAL1/MIS4200Context.cs
@@ -18,9 +18,9 @@ namespace whetstone_MIS4200_MVC_P1.DAL1
         // Include each object here. The value inside <> is the name of the class,
         // the value outside should generally be the plural of the class name
         // and is the name used to reference the entity in code
-        public DbSet<Student> Students { get; set; }
-        public DbSet<Course> Courses { get; set; }
-        public DbSet<CourseDetail> CourseDetails { get; set; }
-        public DbSet<Instructor> Instructors { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Procedure> Procedures  { get; set; }
+        public DbSet<appointmentDetail> appointmentDetails { get; set; }
+        public DbSet<Appointment> appointments { get; set; }
     }
 }
